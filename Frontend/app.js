e09d616404eb16b22e7f779e7bf295b14a774986
Michael <krutiymisha@gmail.com>
2015-02-01 01:16:52 -0500

    added page control flow

--- Frontend/app.js
+++ Frontend/app.js
@@ -1,6 +1,65 @@
 angular.module('Kikets', [])
 
-.controller('loginCtrl', function($scope) {
+.controller('bodyCtrl', function($scope) {
+
+$scope.boolLogin = false;
+$scope.boolSignUp = true;
+$scope.boolBttn = true;
+$scope.boolFriends = true;
+$scope.boolSend = true;
+
+ $scope.loginClick = function () {
+      $scope.boolLogin = true;
+      $scope.boolBttn = false;
+    }
+
+$scope.SignUpClick = function () {
+      $scope.boolSignUp = false;
+      $scope.boolLogin = true;
+    }
+
+$scope.SignUpFinish = function () {
+      $scope.boolSignUp = true;
+      $scope.boolLogin = false;
+    }
+
+$scope.NewKiket = function () {
+      $scope.boolBttn = true;
+      $scope.boolSend = false;
+    }
+
+$scope.AddFriends = function () {
+      $scope.boolBttn = true;
+      $scope.boolFriends = false;
+    }
+
+$scope.SubmitFriends = function () {
+      $scope.boolBttn = false;
+      $scope.boolFriends = true;
+    }
+
+$scope.SubmitSend = function () {
+      $scope.boolBttn = false;
+      $scope.boolSend = true;
+    }
+
+
+});
+
+
+
+
+
+
+
+
+
+
+/*.controller('loginCtrl', function($scope) {
+    
+
+
+
     console.log('hello');
     $scope.name = 'Misha';
     $scope.boolean1 = false;
@@ -18,3 +77,5 @@ angular.module('Kikets', [])
       $scope.boolean1 = !($scope.boolean1);
     }
 });
+
+.controller('signupCtrl')*/
